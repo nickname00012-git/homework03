1a2d8489b3e4047917fd8c6d669367e6a2f5ef03
am <info@malex.su>
2024-10-21 18:54:13 +0300

    First commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Heading
+        int taskN;
+        taskN = 1;
+
+        System.out.println();
+        System.out.println();
+        System.out.println("<<<<  Homework 03  >>>>");
+        System.out.println();
+
+        // Task 1
+
+        System.out.println("Task " + taskN + ".");
+        System.out.println("=======");
+        System.out.println("dog:    " );
+        System.out.println("cat:    " );
+        System.out.println("paper:  " );
+        System.out.println();
+        System.out.println();
+        taskN += 1;
+
+        // Task 2
+
+
     }
 }
